36670edfe37b50a906d11a1fa3ad274750c6e1df
unknown <mchanner@HOXTON-WIN10.idbs.co.uk>
2016-02-29 13:45:07 +0000

    Changed edge detection algorithm to use min,max x and y

--- src/Program.fs
+++ src/Program.fs
@@ -1,13 +1,22 @@
 ﻿open System
 open System.IO
+open System.Linq
 open EdgeDetect.E
 
 [<EntryPoint>]
 let main argv = 
+    
+    let files() = 
+        if argv.Length = 0 then
+            ("..\..\..\data\source", "..\..\..\data\dest")
+        else
+            (argv.[0], argv.[1])
 
-    let InputDir = argv.[0]
-    let OutputDir = argv.[1]
+    let InputDir, OutputDir = files()
 
+    let startTime = System.DateTime.UtcNow
+
+    #if PARALLEL_FILE_LOOP
     Async.Parallel [
         for input_file in Directory.GetFiles(InputDir) ->
             async {
@@ -23,6 +32,25 @@ let main argv =
                 let delta = DateTime.UtcNow - start
 
                 printf "Done in %d(ms) \n" <| int delta.TotalMilliseconds
-            } ]
-    |> Async.RunSynchronously |> ignore
+            }
+    ] |> Async.RunSynchronously |> ignore
+    #else
+    Directory.GetFiles(InputDir) |> Array.iter (fun input_file ->
+            let filename = Path.GetFileName(input_file)
+            let output_file = Path.Combine(OutputDir, filename)
+
+            printf "Processing input file '%s'\n" input_file
+                
+            let start = DateTime.UtcNow
+                
+            process_file input_file output_file MarginSize
+
+            let delta = DateTime.UtcNow - start
+
+            printf "Done in %d(ms) \n" <| int delta.TotalMilliseconds)
+    #endif
+    
+    let elapsed = DateTime.UtcNow - startTime
+    printf "Total time %d(ms) \n" <| int elapsed.TotalMilliseconds
+
     0
--- src/edgedetect.fs
+++ src/edgedetect.fs
@@ -4,13 +4,15 @@ open System
 open System.Drawing
 open System.Drawing.Imaging
 open System.IO
+open System.Linq
+open System.Threading.Tasks
 
 module E =
     [<Literal>]
-    let MarginSize = 40
+    let MarginSize = 10
 
     [<Literal>]
-    let AdaptiveThreshold = 0.9
+    let AdaptiveThreshold = 0.6
 
     /// ensures v is not less than l (lower) and greater than u (upper)
     let clamp l v u = if v < l then l elif v > u then u else v
@@ -19,12 +21,12 @@ module E =
     let xy_to_i w h x y = clamp w (w * y + x) h
 
     /// Calls fn with values x, y and i based on a given array width and height, returning the result as a sequence
-    let seqxy w h fn =
+    let seqxy2 w h =
         seq {
             let pos = ref 0
             for x in 0 .. (w - 1) do
                 for y in 0 .. (h - 1) do
-                    yield fn x y !pos
+                    yield (x, y, !pos)
                     pos := !pos + 1
         }
 
@@ -50,55 +52,36 @@ module E =
         iterxy w h (fun x y i -> img.SetPixel(x, y, pixels.[i]))
         img.Save(file)
 
+    /// Writes an image out to disk with the given width and height
+    let write_image_with_bounds file (w:int) (h:int) (pixels: array<Color>) (rx:int) (ry:int) (rw:int) (rh:int) =
+        let img = new Bitmap(w, h)
+        iterxy w h (fun x y i -> img.SetPixel(x, y, pixels.[i]))
+        Graphics.FromImage(img).DrawRectangle(Pens.Red, rx, ry, rw, rh)
+        img.Save(file)
+
     /// Determines the absolute distance between two points
     let dist x1 y1 x2 y2 = abs (sqrt(float ((pown (x1 - x2) 2) + (pown (y1 - y2) 2))))
 
-    /// Given a binary image, determine the location of the cropped rectangle, returning the
-    /// result as a pair of points representing the top left * bottom right coordinate positions
-    let edge_detect w h (pixels:array<Color>) =
+    let edge_detect2 w h (pixels:array<Color>) =
 
-        let TopLeft, TopRight, BottomLeft, BottomRight = 0, 1, 2, 3
-
-        // the edges of the used area, defaults to the full page
-        let edges = [| (0, 0); (w, 0); (0, h); (w, h) |]
-
-        // keeps track of the distance each point is from the edges of the page.
-        let current = [| for x in 0 .. 3 do yield System.Double.MaxValue |]
-
-        // list of functions to calculate the distance from each corner
-        let distance_funcs = [
-            dist 0 0 (* top left *);
-            dist w 0 (* top right *);
-            dist 0 h (* bottom left *);
-            dist w h (* bottom right *)]
-
+        let mutable minX = w
+        let mutable maxX = 0
+        let mutable minY = h
+        let mutable maxY = 0
+        
         iterxy w h (fun x y index ->
-
             let pixel = pixels.[index]
 
             // if this point is not a background pixel, determine how far away from each corner it is
-            if (int pixel.R) > 0 then
-                // apply x and y to each distance function to determine whether this is the closest point to an edge
-                distance_funcs |> List.iteri (fun i fn ->
-                    let dist = fn x y
-                    if dist < current.[i] then
-                        Array.set edges i (x, y)
-                        Array.set current i dist)
-                ()
-            ())
+            if (int pixel.R) = 0 then
+                if x < minX then minX <- x
+                if x > maxX then maxX <- x
+                if y < minY then minY <- y
+                if y > maxY then maxY <- y
+        )
 
-        let top_left =
-            let x = edges |> Array.map (fun e -> fst e) |> Array.min
-            let y = edges |> Array.map (fun e -> snd e) |> Array.min
-            (x, y)
-
-        let bottom_right =
-            let x = edges |> Array.map (fun e -> fst e) |> Array.max
-            let y = edges |> Array.map (fun e -> snd e) |> Array.max
-            (x, y)
-
-        (top_left, bottom_right)
-
+        ((minX, minY), (maxX, maxY))
+    
     /// Crops an image based on a top left and bottom right coordinates
     let crop srcW srcH x1 y1 x2 y2 (pixels:array<Color>) =
         let destW, destH = (x2 - x1), (y2 - y1)
@@ -109,6 +92,7 @@ module E =
             if x > x1 && x <= x2 && y > y1 && y <= y2 then
                 Array.set output !index pixels.[i]
                 index := !index + 1)
+        
         output
 
     /// Applies adaptive thresholding to an input array, returning the result as a binary image
@@ -130,33 +114,50 @@ module E =
             pixels.[idx x (y + 1)];       // bottom middle
             pixels.[idx (x + 1) (y + 1)]] // bottom right
 
-        // Returns the average colour from a list of colours
+        let pixel_value (c:Color) = ((float (c.R + c.G + c.B)) / 3.0) + 0.1
+
+        // Returns the average grey scale colour from a list of colours
         let average pixels =
             let sum =
                 pixels
-                |> List.map (fun (c:Color) -> c.ToArgb())
+                |> List.map pixel_value
                 |> List.sum
-            (float sum) / (float pixels.Length)
+            sum / (float pixels.Length)
 
         let output = Array.create<Color> (pixels.Length) Color.White
-        iterxy w h (fun x y i ->
-            let pixel = pixels.[i].ToArgb()
+
+        #if PARALLEL_FOREACH
+        let lazy_seq = seqxy2 w h
+        Parallel.ForEach(lazy_seq, fun (x, y, i) -> 
+            let pixel = pixel_value pixels.[i]
             let avg = average <| neighbours x y
-            let diff = float avg / float pixel
+            let diff = ((abs (avg - pixel)) / avg)
+            if diff > threshold then
+                Array.set output i Color.Black) |> ignore
+        #else
+        iterxy w h (fun x y i -> 
+            let pixel = pixel_value pixels.[i]
+            let avg = average <| neighbours x y
+            let diff = ((abs (avg - pixel)) / avg)
             if diff > threshold then
                 Array.set output i Color.Black)
-
-        (w, h, output)
+        #endif
+        output
 
     let process_file inputFile outputFile margins =
         // read the source image into an array of colours
         let (srcW, srcH, source_pixels) = (read_image inputFile)
 
         // convert the image to grey scale and apply adaptive thresholding to create a binary image
-        let (_, _, black_and_white) = adaptive_threshold AdaptiveThreshold srcW srcH source_pixels
+        let black_and_white = adaptive_threshold AdaptiveThreshold srcW srcH source_pixels
+
+        // determine name of intermediate file to write out with debug details 
+        let outputFile2 = Path.Combine(FileInfo(outputFile).Directory.FullName, Path.GetFileNameWithoutExtension(outputFile) + ".bw.png")
 
         // determine where the corners of the visible area are
-        let ((x1, y1), (x2, y2)) = edge_detect srcW srcH (black_and_white |> Array.ofSeq)
+        let ((x1, y1), (x2, y2)) = edge_detect2 srcW srcH (black_and_white |> Array.ofSeq)
+
+        write_image_with_bounds outputFile2 srcW srcH black_and_white x1 y1 (x2 - x1) (y2 - y1)
 
         // add some margin around the corners to give the image some white space (assuming there is room to do so)
         let x1m = if x1 > margins then x1 - margins else x1
